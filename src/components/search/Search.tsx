cbd1cc4609df7643424b95f754b1f0571c438551
Jaehun Kim <crisplaver@gmail.com>
2022-09-11 21:41:06 +0900

    [기능] 검색 페이지 로직 개발
    Co-authored-by: Yang Hong Seok <davidyang2149@gmail.com>
    Co-authored-by: Jaehun Kim <crisplaver@gmail.com>

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -4,7 +4,9 @@ import useSearch from 'src/hooks/search/useSearch';
 
 const Search = () => {
   const {
+    repositoriesData,
     searchInputText,
+    isFetchingRepositoriesData,
     handleChangeSearchText,
     handlePressSearch
   } = useSearch();
@@ -12,14 +14,16 @@ const Search = () => {
     <Fragment>
       <div>
         <input type="text" value={searchInputText} onChange={(e) => handleChangeSearchText(e.target.value)} />
-        <button type="button" onClick={() => handlePressSearch}>검색</button>
+        <button type="button" onClick={handlePressSearch}>검색</button>
       </div>
       <div>
         <ul>
-          <li>결과1</li>
-          <li>결과2</li>
-          <li>결과3</li>
-          <li>결과4</li>
+          {isFetchingRepositoriesData ?
+            '로딩중' :
+            repositoriesData?.items.map(item => (
+              <li>{item.name}</li>
+            ))
+          }
         </ul>
       </div>
     </Fragment>
--- src/hooks/search/useSearch.ts
+++ src/hooks/search/useSearch.ts
@@ -1,21 +1,38 @@
+import axios from 'axios';
 import { atom, useAtom } from 'jotai';
+import useSWR from 'swr';
 
 const searchInputTextAtom = atom<string>('');
+const searchedInputTextAtom = atom<string>('');
 
 const useSearch = () => {
     // React Query 코드 작성
     const [searchInputText, setSearchInputText] = useAtom(searchInputTextAtom);
+    const [searchedInputText, setSearchedInputText] = useAtom(searchedInputTextAtom);
+    
+    const { data: repositoriesData, isValidating: isFetchingRepositoriesData } = useSWR(`https://api.github.com/search/repositories?q=${searchedInputText}`, async () => {
+        const { data } = await axios.get('https://api.github.com/search/repositories', {
+            params: {
+                q: searchedInputText
+            }
+        });
+        return data;
+    }, {
+        revalidateOnMount: false
+    });
 
     const handleChangeSearchText = (text: string) => {
         setSearchInputText(text);
     }
 
     const handlePressSearch = () => {
-        // React Query fetch 액션 작성
+        setSearchedInputText(searchInputText);
     }
 
     return {
+        repositoriesData, 
         searchInputText,
+        isFetchingRepositoriesData,
         handleChangeSearchText,
         handlePressSearch
     }
